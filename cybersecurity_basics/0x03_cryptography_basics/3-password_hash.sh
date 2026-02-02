7dd0b96a3c3fa4a04e40e19614431b5ff3fce924
Your Name <you@example.com>
2026-02-02 21:05:43 +0400

    salam

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-salt=$(openssl rand -base64 12 | cut -c1-16)
-echo -n "$1$salt" | openssl dgst -sha512 > 3_hash.txt
+echo -n "$1$(openssl rand -base64 12 | cut -c1-16)" | openssl dgst -sha512 > 3_hash.txt
