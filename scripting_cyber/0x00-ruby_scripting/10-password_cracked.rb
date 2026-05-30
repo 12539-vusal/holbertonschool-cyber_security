721235eb97f3d883b5a8a4654374b82daa0e290c
Your Name <you@example.com>
2026-05-30 13:24:07 -0400

    salam

--- scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -3,12 +3,12 @@ require 'digest'
 if ARGV.length < 2
   puts "Usage: 10-password_cracked.rb HASHED_PASSWORD DICTIONARY_FILE"
 else
-  hashed = ARGV[0]
-  dictionary = ARGV[1]
+  hashed_password = ARGV[0]
+  dictionary_file = ARGV[1]
   found = false
-  File.foreach(dictionary) do |word|
+  File.foreach(dictionary_file) do |word|
     word = word.chomp
-    if Digest::SHA256.hexdigest(word) == hashed
+    if Digest::SHA256.hexdigest(word) == hashed_password
       puts "Password found: #{word}"
       found = true
       break
